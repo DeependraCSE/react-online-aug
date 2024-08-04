3b7ea5203a5f183a5327780f21ae140abc3b1d2b
React Lab Files <95652257+reactfiles@users.noreply.github.com>
2024-08-04 23:02:10 +0530

    Add files via upload

--- day2/data.js
+++ day2/data.js
@@ -1,4 +1,10 @@
 var odd = [1,3,5,7,9];
 var favnums = [68,63,17,22,32,77,11,13,30,2,16,78,26,5];
 
-export { favnums as fnum }
+// alias name
+// export { favnums as fnum }
+// export { favnums, odd }
+
+// default
+export default favnums
+export { odd }
